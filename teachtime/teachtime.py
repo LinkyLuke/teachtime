fee87cc7ecfeb6b8b0b8a55fdfe0da5afeede239
Luke Coltman <25992603+LinkyLuke@users.noreply.github.com>
2018-07-28 16:00:16 +0100

    Added Flask and layout.html

--- teachtime/teachtime.py
+++ teachtime/teachtime.py
@@ -1,1 +1,22 @@
-teachtime.py
+from flask import Flask, render_template, url_for, flash, redirect
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template('index.html', title='Home')
+
+@app.route("/about")
+def about():
+	return render_template('about.html', title='Home')
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+
+@app.route("/register")
+def register():
+	form = RegisterForm()
+
+if __name__ == '__main__':
+	app.run(debug=True)
