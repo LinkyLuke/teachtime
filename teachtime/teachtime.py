9ff9bfe723bc5d2bd48d578040123a5b2678b210
spacemud <spacemud@users.noreply.github.com>
2018-07-29 22:49:03 +0100

    Move config and route code to separate files, and create a Flask Blueprint for routing purposes
    Also, move application init code to an 'application factory', for later testing purposes

--- teachtime/teachtime.py
+++ teachtime/teachtime.py
@@ -1,42 +1,18 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
+from flask import Flask
 
-app.config['SECRET_KEY'] = 'dansucksomgdansucksseriouslydansucks'
+from teachtime.routes import routes
+from teachtime.config import Config
 
-@app.route("/")
-@app.route("/home")
-def home():
-	return render_template('index.html', title='Home')
+def create_app(config=Config):
+	# Flask initialisation and config
+	app = Flask(__name__)
+	app.config.from_object(config)
 
-@app.route("/about")
-def about():
-	return render_template('about.html', title='Home')
+	# TeachTime blueprints
+	app.register_blueprint(routes)
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		if form.email.data == "admin@teachtime.com" and form.password.data == 'dansucks':
-			flash('You have been logged in!', 'success')
-			return redirect(url_for('home'))
-		else:
-			flash('Login Unsuccessful. Please check username and password', 'danger')
-	return render_template('login.html', title='Login', form=form)
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-	form = RegistrationForm()
-	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}!', 'success')
-		return redirect(url_for('home'))
-	return render_template('register.html', title='Register', form=form)
-
-
-@app.route("/timetable")
-def timetable():
-	return render_template('timetable.html', title="My Timetable")
+	return app
 
 if __name__ == '__main__':
+	app = create_app()
 	app.run(debug=True)
-
