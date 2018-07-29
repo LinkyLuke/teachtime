2e598c8fa2a0256b3c46392179fb19ba01a54f1e
Luke Coltman <25992603+LinkyLuke@users.noreply.github.com>
2018-07-29 12:00:31 +0100

    Improved flash and added register and login

--- teachtime/teachtime.py
+++ teachtime/teachtime.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dansucksomgdansucksseriouslydansucks'
 
 @app.route("/")
 @app.route("/home")
@@ -10,13 +13,25 @@ def home():
 def about():
 	return render_template('about.html', title='Home')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == "admin@teachtime.com" and form.password.data == 'dansucks':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check username and password', 'danger')
+	return render_template('login.html', title='Login', form=form)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-	form = RegisterForm()
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
