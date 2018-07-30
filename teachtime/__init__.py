4b9ea4c480cb1f1c61d733d3b86f0f772159b221
Luke Coltman <25992603+LinkyLuke@users.noreply.github.com>
2018-07-30 22:40:57 +0100

    Added user registration and login

--- teachtime/__init__.py
+++ teachtime/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 from teachtime.config import Config
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfajksbhgvikhjeuradhgbfiejrwbf4347y84784e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from teachtime.routes import routes
 
