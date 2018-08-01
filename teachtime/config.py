3b1de2714ec104d3c9c0e17bf0d2788f06914eea
spacemud <spacemud@users.noreply.github.com>
2018-08-01 20:20:44 +0100

    Update config

--- teachtime/config.py
+++ teachtime/config.py
@@ -2,3 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'change_me'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    LOGIN_VIEW = 'login'
+    LOGIN_MESSAGE_CATEGORY = 'info'
