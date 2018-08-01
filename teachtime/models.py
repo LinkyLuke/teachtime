48538b801a7860ead308fcf6b1975c7498fb6952
spacemud <spacemud@users.noreply.github.com>
2018-08-01 22:52:28 +0100

    Add basic event model

--- teachtime/models.py
+++ teachtime/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
 	password = db.Column(db.String(60), nullable = False)
-	timetables = db.relationship('Timetable', backref='author', lazy=True)
+	timetables = db.relationship('Timetable', backref='user', lazy=True)
 
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -23,11 +23,19 @@ class User(db.Model, UserMixin):
 class Timetable(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(200), nullable=False)
-	date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	content = db.Column(db.Text, nullable=False)
-	#file = db.File?()
+	events = db.relationship('Event', backref='timetable', lazy=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 	def __repr__(self):
-		return f"Timetable('{self.title}', '{self.date}', '{self.time}', '{self.content}')"
+		return f"Timetable('{self.title}',  '{self.content}')"
+
+
+class Event(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(200), nullable=False)
+	time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	content = db.Column(db.Text, nullable=False)
+	timetable_id = db.Column(db.Integer, db.ForeignKey('timetable.id'), nullable=False)
+
+	def __repr__(self):
+		return f"Event('{self.title}', '{self.time}', '{self.content}')"
