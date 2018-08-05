ffb9cd152cbb557cb417f631da1406987f35352a
spacemud <spacemud@users.noreply.github.com>
2018-08-05 17:48:15 +0100

    Add custom converters

---
+++ teachtime/util.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from flask import abort
+from werkzeug.routing import BaseConverter, ValidationError
+
+def dan_strftime(format, date):
+    return date.strftime(format.replace('%o', {1: 'st', 2: 'nd', 3: 'rd'}.get(date.day if (date.day < 20) else (date.day % 10), 'th')))
+
+class ViewConverter(BaseConverter):
+    """Extracts a view string from the path and validates it""" # Questionable whether this belongs as a converter...
+    
+    def to_python(self, value):
+        views = ('day', 'month', 'year')
+
+        if value in views:
+            return value
+
+        return abort(404)
+
+    def to_url(self, value):
+        return value
+
+class DateConverter(BaseConverter):
+    """Extracts a ISO8601 date from the path and validates it"""
+
+    def to_python(self, value):
+        try:
+            return datetime.strptime(value, '%Y-%m-%d').date()
+        except ValueError:
+            raise ValidationError()
+
+    def to_url(self, value):
+        return value.strftime('%Y-%m-%d')
