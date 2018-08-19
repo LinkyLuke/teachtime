3a0d9ea59afd21dc4a5f67fee30f4958d12be51e
spacemud <spacemud@users.noreply.github.com>
2018-08-19 16:42:45 +0100

    Protect timetable routes with login_required decorator, rename view functions

--- teachtime/timetables/routes.py
+++ teachtime/timetables/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, render_template, redirect, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 from teachtime.timetables.forms import CreateTimetableForm, EditTimetableForm
 from teachtime.models import Timetable
@@ -8,17 +8,20 @@ from teachtime.teachtime import db
 timetables = Blueprint('timetables', __name__)
 
 @timetables.route('/timetable')
-def index():
+@login_required
+def list_timetables():
     timetables = Timetable.query.all()
     return render_template('timetables/index.html', title="Timetables", timetables=timetables)
 
 @timetables.route('/timetable/create')
-def create():
+@login_required
+def create_timetable():
     form = CreateTimetableForm()
     return render_template('timetables/create.html', title="Create a timetable", form=form)
 
 @timetables.route('/timetable', methods=['POST'])
-def store():
+@login_required
+def store_timetable():
     form = CreateTimetableForm()
 
     if form.validate_on_submit():
@@ -30,15 +33,17 @@ def store():
         db.session.add(timetable)
         db.session.commit()
 
-        return redirect(url_for('timetables.index'))
+        return redirect(url_for('timetables.store_timetable'))
 
 @timetables.route('/timetable/<int:id>')
-def show(id):
+@login_required
+def show_timetable(id):
     timetable = Timetable.query.get(id)
     return render_template('timetables/show.html', title=f"Timetable {timetable.title}", timetable=timetable)
 
 @timetables.route('/timetable/<int:id>')
-def edit(id):
+@login_required
+def edit_timetable(id):
     timetable = Timetable.query.get_or_404(id)
     form = EditTimetableForm()
 
@@ -51,5 +56,6 @@ def edit(id):
     return render_template('timetables/edit.html', title=f"Timetable {timetable.title}", timetable=timetable, form=form)
 
 @timetables.route('/timetable/<int:id>/events/create')
+@login_required
 def create_event(id):
     pass
