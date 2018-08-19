473eccfe7d249e8f5e30e3e72b7a011b9d8d68a7
spacemud <spacemud@users.noreply.github.com>
2018-08-19 13:31:28 +0100

    Add timetable forms

---
+++ teachtime/timetables/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateTimeField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
+
+class CreateTimetableForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=20)])
+    submit = SubmitField('Submit')
+
+class EditTimetableForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=20)])
+    time = DateTimeField('Start time')
+    submit = SubmitField('Submit')
+
+class CreateEventForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=20)])
+    description = StringField('Description')
+    time = DateTimeField('Time', validators=[DataRequired()])
+    submit = SubmitField('Submit')
