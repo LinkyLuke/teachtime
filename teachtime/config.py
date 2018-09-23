52c061b4362ef9a9538c2f2d04f4f329465f76b6
spacemud <spacemud@users.noreply.github.com>
2018-09-23 13:06:06 +0100

    Update config

--- teachtime/config.py
+++ teachtime/config.py
@@ -1,8 +1,18 @@
 import os
 
 class Config:
+	DEBUG = False
+	TESTING = False
+
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'change_me'
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+
+	# TeachTime
+	DATABASE = 'site.db'
+
+	# Flask-SQLAlchemy
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE
+
+	# Flask-Login
 	LOGIN_VIEW = 'users.login'
 	LOGIN_MESSAGE_CATEGORY = 'info'
 
@@ -12,3 +22,13 @@ class Config:
 	MAIL_USE_TLS = 'True'
 	MAIL_USERNAME = os.environ.get('EMAIL_USER')
 	MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+class ProductionConfig(Config):
+	pass
+
+class DevelopmentConfig(Config):
+	DEBUG = True
+
+class TestingConfig(Config):
+	TESTING = True
+	DATABASE = 'test.db'
